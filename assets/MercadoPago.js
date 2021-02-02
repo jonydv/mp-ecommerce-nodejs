dfb3b06e16af23b044c1e000a7412c7b1886fce1
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2021-02-02 15:36:34 -0300

    fixed home view and detail

--- assets/MercadoPago.js
+++ assets/MercadoPago.js
@@ -1,5 +1,4 @@
-window.onload(
-    window.Mercadopago.setPublishableKey('APP_USR-d81f7be9-ee11-4ff0-bf4e-20c36981d7bf')
-);
+window.Mercadopago.setPublishableKey('APP_USR-d81f7be9-ee11-4ff0-bf4e-20c36981d7bf');
+
 
 
