a2fb3827bb1ca6570be401017d1c8452f5de45e5
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2021-02-02 15:01:59 -0300

    fixed publishable key

--- assets/MercadoPago.js
+++ assets/MercadoPago.js
@@ -1,2 +1,5 @@
-window.Mercadopago.setPublishableKey('APP_USR-d81f7be9-ee11-4ff0-bf4e-20c36981d7bf');
+window.onload(
+    window.Mercadopago.setPublishableKey('APP_USR-d81f7be9-ee11-4ff0-bf4e-20c36981d7bf')
+);
+
 
